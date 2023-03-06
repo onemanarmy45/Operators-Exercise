c030b4b71e2479041b99c225b50b508760040f9e
rob.jordan054@gmail.com <rob.jordan054@gmail.com>
2023-03-06 13:56:55 -0500

    operators

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int a = 17;
+            int b = 4;
+
+            Console.WriteLine(a + b);
+
+            Console.WriteLine(a - b);
+
+            Console.WriteLine(a * b);
+
+            Console.WriteLine(a / b);
+
+            Console.WriteLine(a % b);
+
+            int quo = (a / b);
+            int rem = (a % b);
+
+            Console.WriteLine($"{a} / {b} is {quo} with a remainder of {rem}");
+
+            Console.WriteLine($"AreaOfCircle");
+
+            Console.WriteLine($"What is the radius of your circle?");
+
+            var radius = double.Parse(Console.ReadLine());
+            double area = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine();
+            Console.WriteLine($"The area of a circle with radius of {radius} is {area}");
+
         }
     }
 }
